ed8edb7e4242c74a27cd30947be24214a2d19a81
bikilone <milosbirovljevic@gmail.com>
2018-05-15 15:03:43 +0200

    main page style finished

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Router, Switch, Redirect } from "react-router-dom"
+import { Route, Switch, Redirect } from "react-router-dom"
 import '../App.css';
 
 import Header from "./partials/header";
 import LandingPage from "./landing_page/landingPage";
+import MainPage from "./main_page/mainPage"
 
 
 class App extends Component {
@@ -16,12 +17,12 @@ class App extends Component {
         </header>
         <main className="container">
           <div className="row">
-        <LandingPage/>
+        {/* <LandingPage/> */}
           </div>
         </main>
-      {/* <Switch>
-         
-      </Switch> */}
+         <Redirect from="/" to="/landingPage"/>
+         <Route path="/mainPage" component={MainPage}/>
+         <Route path="/landingPage" component={LandingPage}/>
         </React.Fragment>
     );
   }
