816c5a06ddf760c46a3640db01cf8d6606cd80d3
bikilone <milosbirovljevic@gmail.com>
2018-05-15 10:57:29 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
@@ -13,9 +15,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+        </div>
     );
   }
 }
 
 export default App;
+
+
+
+  
+
+ 
